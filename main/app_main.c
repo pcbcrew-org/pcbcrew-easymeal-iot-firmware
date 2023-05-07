ab520845cfb7dcbb2e650bf36d77b5f11918c301
BadTux <tekobase@outlook.com>
2023-05-08 04:36:46 +0800

    remove Arduino componenet, remove RTC.

--- main/app_main.c
+++ main/app_main.c
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 
 #include "esp_log.h"
-#include "Arduino.h"
 
 #include "storage.c"
 #include "button.c"
 #include "router.c"
-#include "rtc.c"
+// #include "rtc.c"
 #include "webserver.c"
 
 static const char *TAG_MAIN = "pcbcrew-router-main";
@@ -19,16 +18,8 @@ void app_main(void)
     esp_log_level_set("*", ESP_LOG_INFO);
 
     setup_storage();
-    setup_rtc();
+    // setup_rtc();
     setup_button();
     setup_router();
     setup_webserver();
-
-    initArduino();
-    pinMode(9, OUTPUT);
-    digitalWrite(9, HIGH);
-
-    while (true) {
-        //
-    }
 }
